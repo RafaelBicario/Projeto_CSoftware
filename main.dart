9ca8a0d82cd2c500e680cbe7a2466508db5d02c3
RafaelBicario <rafabicario@outlook.com>
2021-07-05 21:24:46 -0300

    TDD

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'test.dart';
+
+void main() {
+  test('empty email returns error string', () {
+    final result = validarEmail('');
+    expect(result, 'Email can\'t be empty');
+  });
+
+  test('non-empty email returns null', () {
+    final result = validarEmail('email');
+    expect(result, null!);
+  });
+
+  test('empty password returns error string', () {
+    final result = validarPassword('');
+    expect(result, 'Password can\'t be empty');
+  });
+
+  test('non-empty password returns null', () {
+    final result = validarPassword('password');
+    expect(result, null!);
+  });
+}
+
+void expect(result, String s) {}
+
+void test(String s, Null Function() param1) {}
---
+++ test.dart
@@ -0,0 +1,7 @@
+String? validarEmail(String value) {
+  return value.isEmpty ? 'Email não pode estar vazio!!!' : null;
+}
+
+String? validarPassword(String value) {
+  return value.isEmpty ? 'Password não pode estar vazio!!!' : null;
+}
